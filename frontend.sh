d3c360cb824c6c4a1200ad94a7b14983530b4aed
chava venkat <chavavrajesh1.com>
2024-04-20 16:29:47 +0530

    hello script

--- frontend.sh
+++ frontend.sh
@@ -8,17 +8,15 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-# echo "Please enter DB password"
-# read -s mysql_root_password
 
 VALIDATE(){
-if [ $1 -ne 0 ]
-then
-    echo -e "$2...$R FAILURE $N"
-    exit 1
-else
-    echo -e "$2...$G SUCCESS $N"
-fi
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
 }
 
 if [ $USERID -ne 0 ]
@@ -26,19 +24,19 @@ then
     echo "Please run this script with root access."
     exit 1 # manually exit if error comes.
 else
-    echo "You are super user"
-fi 
+    echo "You are super user."
+fi
 
 dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "Installing nginx"
 
-systemctl enable nginx
-VALIDATE $? "Enable nginx"
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "Enabling nginx"
 
-systemctl start nginx
+systemctl start nginx &>>$LOGFILE
 VALIDATE $? "Starting nginx"
 
-rm -rg /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
 VALIDATE $? "Removing existing content"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
